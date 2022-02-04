363f4d8cb7f1f6cc14629e1a915a528b9843e772
pid <pid@hebe.ch>
2022-02-04 13:46:44 +0100

    Autoupdate: 20224604-130244

--- git_update.sh
+++ git_update.sh
@@ -4,3 +4,9 @@ git add --all
 commit=$(date +%Y%M%d-%H%m%S)
 git commit -m "Autoupdate: $commit"
 git push -u origin main
+
+
+
+# For submodules:
+# Install: git submodule add https://github.com/nanxiaobei/hugo-paper themes/paper
+# The run: git submodule update --init --recursive
